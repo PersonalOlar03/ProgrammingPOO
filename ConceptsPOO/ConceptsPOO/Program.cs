78d1dc48f16837e838d760b0dc7da9e3003baad6
VICTOR OLIVEROS LARREA <victoroli3@outlook.com>
2022-02-12 11:18:10 -0500

    Finish Date Class

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConceptsPOO;
+
+try
+{
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine("=============");
+    Console.WriteLine(new Date(2021, 2, 12));
+    Console.WriteLine(new Date(2002, 1, 13));
+    Console.WriteLine(new Date(1998, 4, 3));
+    Console.WriteLine(new Date(2024, 13, 25));
+}
+catch (Exception error)
+{
+
+    Console.WriteLine(error.Message);
+
+}
+
+
